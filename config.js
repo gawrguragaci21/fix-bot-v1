ec6d8f8921d710df54b28f3806fa21bc54af8dbb
gawrguragaci21 <108918592+gawrguragaci21@users.noreply.github.com>
2022-09-23 19:46:53 +0700

    Update config.js

--- config.js
+++ config.js
@@ -119,10 +119,10 @@ global.author = '                「 Hexzrky あ」'
 
 
 /*============== LOGO ==============*/
-global.thumb = 'https://i.ibb.co/XZrK6yQ/transformers.jpg' //Main Thumbnail
-global.imagebot = 'https://i.ibb.co/XZrK6yQ/transformers.jpg'
+global.thumb = 'src/bansos.jpg' //Main Thumbnail
+global.imagebot = 'src/bansos.jpg'
 global.giflogo = 'https://telegra.ph/file/a46ab7fa39338b1f54d5a.mp4'
-global.thumbs = ['https://i.ibb.co/XZrK6yQ/transformers.jpg']
+global.thumbs = ['src/bansos.jpg']
 global.fla = 'https://i.ibb.co/XZrK6yQ/transformers.jpg'
 global.flaaa2 = [
 'https://flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=water-logo&script=water-logo&fontsize=90&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&fillTextColor=%23000&shadowGlowColor=%23000&backgroundColor=%23000&text=',
